f8249624818f7d163e1b5d63a64fcea6e090a553
DawidKluzowski <kluzowski.dawid@gmail.com>
2023-11-22 19:16:13 +0100

    add card component and basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import ActivityList from '@/components/ActivityList';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { useEffect, useState } from 'react';
+import { AcivitiesList } from '@/types';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const DUMMY_TABLES: Table[] = [
+const DUMMY_Activities: AcivitiesList[] = [
     {
         id: uuidv4(),
         name: 'todo',
@@ -30,26 +31,15 @@ const DUMMY_TABLES: Table[] = [
     },
 ];
 
-interface Activity {
-    id: string;
-    name: string;
-    description: string;
-}
-
-interface Table {
-    id: string;
-    name: string;
-    activities: Activity[];
-}
-
 export default function Home() {
-    const [tables, setTables] = useState<Table[]>(DUMMY_TABLES);
-    const [tablesEditMap, setTableEditMap] = useState<
-        { id: string; isEdit: boolean }[]
-    >([]);
+    const [tables, setTables] = useState<AcivitiesList[]>(DUMMY_Activities);
+
+    const removeTable = (id: string) => {
+        setTables(tables.filter((table) => table.id !== id));
+    };
 
     const addTable = () => {
-        const newTable: Table = {
+        const newTable: AcivitiesList = {
             id: uuidv4(),
             name: 'new table',
             activities: [],
@@ -57,70 +47,16 @@ export default function Home() {
         setTables((prev) => [...prev, newTable]);
     };
 
-    const removeTable = (id: string) => {
-        setTables(tables.filter((table) => table.id !== id));
-    };
-
-    useEffect(() => {
-        setTableEditMap(
-            tables.map((table) => ({ id: table.id, isEdit: false }))
-        );
-    }, [tables]);
-
-    const toogleEdit = (id: string, vlaue: boolean) => {
-        setTableEditMap((prev) =>
-            prev.map((p) => {
-                if (p.id === id) {
-                    p.isEdit = vlaue;
-                }
-                return p;
-            })
-        );
-    };
-
     return (
         <main className="flex min-h-screen flex-col gap-5 p-24">
             <div className="flex gap-8 bg-fuchsia-400">
                 {tables.map((table) => {
-                    const isEdit = tablesEditMap.find(
-                        (tableEdit) => tableEdit.id === table.id
-                    )?.isEdit;
-
                     return (
-                        <div className=" bg-green-600" key={table.id}>
-                            <span>{table.name}</span>
-                            <Button onClick={() => removeTable(table.id)}>
-                                RM
-                            </Button>
-                            <div className="bg-sky-600">
-                                {table.activities.map((activity) => (
-                                    <div key={activity.id}>{activity.name}</div>
-                                ))}
-                                {!isEdit ? (
-                                    <Button
-                                        onClick={() =>
-                                            toogleEdit(table.id, true)
-                                        }
-                                    >
-                                        +
-                                    </Button>
-                                ) : (
-                                    <>
-                                        <Input />
-                                        <Button>add</Button>
-                                        {isEdit && (
-                                            <Button
-                                                onClick={() =>
-                                                    toogleEdit(table.id, false)
-                                                }
-                                            >
-                                                Exit
-                                            </Button>
-                                        )}
-                                    </>
-                                )}
-                            </div>
-                        </div>
+                        <ActivityList
+                            removeTable={() => removeTable(table.id)}
+                            key={table.id}
+                            activitiesList={table}
+                        />
                     );
                 })}
                 <div className=" bg-green-600">
