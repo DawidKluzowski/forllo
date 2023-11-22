250bc82e3baa1d994243b3a2eef67704f14e3594
DawidKluzowski <kluzowski.dawid@gmail.com>
2023-11-22 15:12:53 +0100

    add pettier and redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,28 @@
 import type { Metadata } from 'next'
-import { Inter, Libre_Franklin } from 'next/font/google'
+import { Libre_Franklin } from 'next/font/google'
 import { Providers } from '../app/GlobalRedux/provider'
 import './globals.css'
 
-
 const libreFranklin = Libre_Franklin({
-  subsets: ['latin'],
-  variable: '--font-franklin',
-});
+    subsets: ['latin'],
+    variable: '--font-franklin',
+})
 
 export const metadata: Metadata = {
-  title: 'Forllo',
-  description: 'like trello but one more',
+    title: 'Forllo',
+    description: 'like trello but one more',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={libreFranklin.className}>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={libreFranklin.className}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    )
 }
