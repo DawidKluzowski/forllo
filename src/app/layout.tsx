4106f791e8f76a3c1e9a5457ef398fe2b43962b2
DawidKluzowski <kluzowski.dawid@gmail.com>
2024-05-07 18:21:52 +0200

    remove comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import type { Metadata } from 'next';
 import { Libre_Franklin } from 'next/font/google';
 import { Providers } from './StoreProvider';
 import './globals.css';
@@ -10,11 +9,6 @@ const libreFranklin = Libre_Franklin({
     subsets: ['latin'],
     variable: '--font-franklin',
 });
-
-// export const metadata: Metadata = {
-//     title: 'Forllo',
-//     description: 'like trello but one more',
-// };
 
 export default function RootLayout({
     children,
