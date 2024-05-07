77e4914e32d57a758a0d744aa5cf12a82d183a88
DawidKluzowski <kluzowski.dawid@gmail.com>
2024-05-07 18:18:02 +0200

    add basic navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
+'use client';
+
 import type { Metadata } from 'next';
 import { Libre_Franklin } from 'next/font/google';
 import { Providers } from './StoreProvider';
 import './globals.css';
+import { Navbar } from '@/components/Navbar';
 
 const libreFranklin = Libre_Franklin({
     subsets: ['latin'],
     variable: '--font-franklin',
 });
 
-export const metadata: Metadata = {
-    title: 'Forllo',
-    description: 'like trello but one more',
-};
+// export const metadata: Metadata = {
+//     title: 'Forllo',
+//     description: 'like trello but one more',
+// };
 
 export default function RootLayout({
     children,
@@ -21,7 +24,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={libreFranklin.className}>
-                <Providers>{children}</Providers>
+                <Providers>
+                    <Navbar />
+                    {children}
+                </Providers>
             </body>
         </html>
     );
